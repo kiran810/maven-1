be182ddf2379453971137437d30795371259c570
kiran810 <59352900+kiran810@users.noreply.github.com>
2020-03-17 01:13:29 +0530

    Update MultiplyTwoNumbers.java

--- src/main/java/mavenproject1/MultiplyTwoNumbers.java
+++ src/main/java/mavenproject1/MultiplyTwoNumbers.java
@@ -3,7 +3,7 @@ public class MultiplyTwoNumbers {
     public static void main(String[] args) {
         float first = 1.5f;
         float second = 2.0f;
-        float product = first * seond;
+        float product = first * sond;
         System.out.println("The product is: " + product);
     }
 }
